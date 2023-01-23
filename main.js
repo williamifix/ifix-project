4ab5b9ef1f62622298e55a96aa57a9ee1baed1a5
WILLIAM ETORNAM <wgoku885@gmail.com>
2023-01-23 00:39:21 -0800

    this is a massive update running through the nav menu icon and and its on-click event from the main.js file

---
+++ main.js
@@ -0,0 +1,14 @@
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+
+function togglemenu() {
+    if (menuList.style.maxHeight == "0px") {
+        menuList.style.maxHeight = "300px";
+
+    }
+    else {
+        menuList.style.maxHeight = "0px";
+    }
+}
