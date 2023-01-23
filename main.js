3ad917a5a6a69cb991388ac0c0c2bb5de38cd44a
WILLIAM ETORNAM <wgoku885@gmail.com>
2023-01-23 02:55:11 -0800

    updated css

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 var menuList = document.getElementById("menuList");
-
+var menuIconTop = document.getElementById("menuIconTop");
+var menuIconMiddle = document.getElementById("menuIconMiddle");
+var menuIconBottom = document.getElementById("menuIconBottom");
 menuList.style.maxHeight = "0px";
 
 
 function togglemenu() {
     if (menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "300px";
+        menuIconBottom.style.transform = "rotate(45deg)";
+        menuIconMiddle.style.display = "none";
+        menuIconTop.style.transform = "rotate(-45deg)";
+
 
     }
     else {
         menuList.style.maxHeight = "0px";
     }
+
 }
