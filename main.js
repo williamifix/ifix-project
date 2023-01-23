9b2483016fcd9c3f14e60cf71370a41b3604f998
WILLIAM ETORNAM <wgoku885@gmail.com>
2023-01-23 10:41:58 -0800

    updated the menu-icon toggle activity

--- main.js
+++ main.js
@@ -8,14 +8,19 @@ menuList.style.maxHeight = "0px";
 function togglemenu() {
     if (menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "300px";
-        menuIconBottom.style.transform = "rotate(45deg)";
-        menuIconMiddle.style.display = "none";
         menuIconTop.style.transform = "rotate(-45deg)";
+        menuIconMiddle.style.display = "none";
+        menuIconBottom.style.transform = "rotate(45deg)";
 
 
     }
     else {
         menuList.style.maxHeight = "0px";
+        menuIconTop.style.transform = "rotate(180deg)";
+        menuIconMiddle.style.display = "block";
+        menuIconBottom.style.transform = "rotate(180deg)";
+
     }
+
 
 }
